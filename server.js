9951601bdd899381be5c36bc727a859009cf8f75
Chuu <99422689+Chuu5@users.noreply.github.com>
2022-07-10 23:56:08 -0300

    trying to deploy a fake api

---
+++ server.js
@@ -0,0 +1,19 @@
+const jsonserver = require("json-server");
+const server = jsonserver.create();
+const router = jsonserver.router("./db.json");
+const middlewares = jsonserver.defaults({
+    static: "./build"
+})
+
+const port = process.env.PORT || 5000;
+server.use(middlewares);
+server.use(
+    jsonserver.rewriter({
+        "/api/*": "/$1",
+    })
+)
+
+server.use(router)
+server.listen(port, () => {
+    console.log("server is running on " + port )
+})
